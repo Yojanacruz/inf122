3eaed2392d2b6554d6acb8c4efc14d30386bc3b1
Yojanacruz <155459565+Yojanacruz@users.noreply.github.com>
2024-01-08 14:42:14 +0000

    mi primer comint

---
+++ script.js
@@ -0,0 +1,7 @@
+function agregarTarea() {
+    var nuevaTarea = document.getElementById("nueva-tarea").value;
+    var nuevaLista = document.createElement("li");
+    nuevaLista.textContent = nuevaTarea;
+    document.getElementById("tareas-lista").appendChild(nuevaLista);
+    document.getElementById("nueva-tarea").value = "";
+}
