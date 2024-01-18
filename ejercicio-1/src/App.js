2638e93361bc05dba0690cb69deb45744b80d380
Yojanacruz <155459565+Yojanacruz@users.noreply.github.com>
2024-01-18 11:04:01 +0000

    commit

---
+++ ejercicio-1/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  
+
+  const [show, setShow] = useState(true);
+
+  return (
+    <div className="container">
+      <button onClick={() => setShow((show) => !show)}> Show / Hide</button>
+      {show ? <h1>Welcome to React Challenges</h1> : null}
+    </div>
+  );
+}
